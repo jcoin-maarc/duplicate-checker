6f4745aa0aa4a28bec3959a961cdf1628f183877
Phil Schumm <pschumm@uchicago.edu>
2023-03-16 05:09:18 -0500

    Add command to load already enrolled participants

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_login import login_required, logout_user, current_user
 from config import Config
 from .models import db, login_manager, User, Participant
 from .oauth import blueprint
-from .cli import create_db
+from .cli import create_db, load_participants
 from flask_bootstrap import Bootstrap4
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, RadioField, SubmitField, ValidationError
@@ -24,6 +24,7 @@ app.config.from_object(Config)
 # )
 app.register_blueprint(blueprint, url_prefix='/login')
 app.cli.add_command(create_db)
+app.cli.add_command(load_participants)
 db.init_app(app)
 login_manager.init_app(app)
 bootstrap = Bootstrap4(app)
--- app/cli.py
+++ app/cli.py
@@ -1,10 +1,39 @@
 import click
 from flask.cli import with_appcontext
 from .models import db
+import csv
+from .models import db, Participant
+from datetime import date, datetime
 
 @click.command(name="createdb")
 @with_appcontext
 def create_db():
+    """Create database tables."""
     db.create_all()
     db.session.commit()
     print("Database tables created")
+
+@click.command(name="load")
+@click.option('--path', prompt='Path to csv file',
+              help='File containing participants to load.')
+@with_appcontext
+def load_participants(path):
+    """Load one or more already enrolled participants from CSV file."""
+    from . import format_info
+    cnt = 0
+    with open(path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            row['dob'] = date.fromisoformat(row['dob'])
+            info = format_info(first_initial=row['first_initial'],
+                               last_initial=row['last_initial'],
+                               dob=row['dob'],
+                               sex=row['sex'])
+            participant = Participant(info=info,
+                                      recruitment_date=datetime.fromisoformat(row['recruitment_date']),
+                                      recruited_by=row['recruited_by'])
+            db.session.add(participant)
+            cnt += 1
+
+    db.session.commit()
+    print(f'{cnt} records loaded')
